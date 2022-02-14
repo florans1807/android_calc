f9c3961c8f81e0ed506d3d025802882eecfcdf1d
iakov <iakovlevjenya21@mail.ru>
2022-02-14 21:33:02 +0300

    Initial commit

---
+++ app/src/main/java/com/example/voice_calculator/MainActivity.java
@@ -0,0 +1,226 @@
+package com.example.voice_calculator;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.os.Bundle;
+import android.speech.RecognizerIntent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import net.objecthunter.exp4j.Expression;
+import net.objecthunter.exp4j.ExpressionBuilder;
+
+import java.util.ArrayList;
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+
+    //IDs for all the numeric buttons
+    private int[] numButtons = {R.id.btnZero, R.id.btnOne, R.id.btnTwo, R.id.btnThree
+            , R.id.btnFour,
+    R.id.btnFive, R.id.btnSix, R.id.btnSeven, R.id.btnEight, R.id.btnNine};
+
+    private int[] opButtons = {R.id.plus, R.id.minus, R.id.btnMultiply, R.id.btnDivide};
+
+    private TextView txtScreen;
+    private boolean lastNumber;
+    private boolean stateError;
+    private boolean lastDot;
+    private ImageButton btnSpeak;
+    private final int REQ_CODE_SPEECH_INPUT = 100;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnSpeak = findViewById(R.id.speak);
+        txtScreen = findViewById(R.id.txtScreen);
+
+        //find and set onclicklistener to number buttons
+        setNumberOnClickListener();
+
+        //find and set onclicklistener to operator buttons
+        setOperatorOnClickListener();
+
+    }
+
+    private void setNumberOnClickListener() {
+        View.OnClickListener listener = new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                //just append/set the text of clicked button
+                Button button = (Button) view;
+                if (stateError) {
+                    //if current state is error, replace the error message
+                    txtScreen.setText(button.getText());
+                    stateError = false;
+                } else {
+                    txtScreen.append(button.getText());
+                }
+                //set the FLAG
+                lastNumber = true;
+            }
+        };
+
+        //assign the listener to all the number button
+        for (int id : numButtons) {
+            findViewById(id).setOnClickListener(listener);
+        }
+    }
+
+    private void setOperatorOnClickListener() {
+        View.OnClickListener listener = new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                //if the current state is error do not append the operator
+                //if last input is number only, append the operator
+                if (lastNumber && !stateError) {
+                    Button button = (Button) view;
+                    txtScreen.append(button.getText());
+                    lastNumber = false;
+                    lastDot = false; //reset the dot flag
+                }
+            }
+        };
+
+        //assign the listener to all the operator button
+        for (int i : opButtons) {
+            findViewById(i).setOnClickListener(listener);
+        }
+
+        //decimal point
+        findViewById(R.id.btnDot).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (lastNumber && !stateError && !lastDot) {
+                    txtScreen.append(".");
+                    lastNumber = false;
+                    lastDot = false;
+                }
+            }
+        });
+
+        //clear button
+        findViewById(R.id.clearBtn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                txtScreen.setText("");
+                lastNumber = false;
+                stateError = false;
+                lastDot = false;
+            }
+        });
+
+        //equal button
+        findViewById(R.id.equalBtn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onEqual();
+            }
+        });
+
+        //speak button
+        findViewById(R.id.speak).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (stateError) {
+                    txtScreen.setText("try again");
+                    stateError = false;
+                } else {
+                    promptSpeechInput();
+                }
+                lastNumber = true;
+            }
+        });
+
+    }
+
+    private void onEqual() {
+        if (lastNumber && !stateError) {
+            String txt = txtScreen.getText().toString();
+
+            //create an expression
+            try {
+                Expression expression = null;
+                try {
+                    expression = new ExpressionBuilder(txt).build();
+                    double result = expression.evaluate();
+                    txtScreen.setText(Double.toString(result));
+                } catch (Exception e) {
+                    txtScreen.setText("Error");
+                    stateError = true;
+                    lastNumber = false;
+                }
+            } catch (ArithmeticException ex) {
+                txtScreen.setText("Error");
+                stateError = true;
+                lastNumber = false;
+            }
+        }
+    }
+
+    private void promptSpeechInput() {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT
+                , getString(R.string.speech_prompt));
+
+        try {
+            startActivityForResult(intent, REQ_CODE_SPEECH_INPUT);
+        } catch (ActivityNotFoundException a){
+            Toast.makeText(getApplicationContext(), getString(R.string.speech_not_supported)
+                    , Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    //receiving speech input
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode) {
+            case REQ_CODE_SPEECH_INPUT:{
+                if (resultCode == RESULT_OK && data != null) {
+
+                    ArrayList<String> result = data
+                            .getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                    String change = result.get(0);
+                    change =  change.replace("x", "*");
+                    change =  change.replace("X", "*");
+                    change =  change.replace("add", "+");
+                    change =  change.replace("plus", "+");
+                    change =  change.replace("sub", "-");
+                    change =  change.replace("minus", "-");
+                    change =  change.replace("to", "2");
+                    change =  change.replace("multiply", "*");
+                    change =  change.replace("divide", "/");
+                    change =  change.replace("equal", "=");
+                    change =  change.replace("equals", "=");
+
+                    if (change.contains("=")) {
+                        change = change.replace("=", "");
+                        txtScreen.setText(change);
+                        onEqual();
+                    } else {
+                        txtScreen.setText(change);
+                    }
+
+                }
+                break;
+            }
+        }
+    }
+
+}
